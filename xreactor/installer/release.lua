d731c87a1d832e5ce91d6ec6a9965fdef00c5c3b
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-02-01 00:07:49 +0100

    Improve installer storage handling

--- xreactor/installer/release.lua
+++ xreactor/installer/release.lua
@@ -2,7 +2,7 @@ return {
   commit_sha = "beta",
   hash_algo = "crc32",
   manifest_path = "xreactor/installer/manifest.lua",
-  installer_core_version = "1.9",
-  installer_core_hash = "9f8951cd",
-  installer_core_size_bytes = 102959
+  installer_core_version = "2.0",
+  installer_core_hash = "2a946662",
+  installer_core_size_bytes = 110684
 }
