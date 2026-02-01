d96e74c866396469063617fe8d5c520c01ec4592
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-02-01 20:59:54 +0100

    Refine disk-first installs and streaming downloads

--- xreactor/installer/release.lua
+++ xreactor/installer/release.lua
@@ -2,7 +2,7 @@ return {
   commit_sha = "beta",
   hash_algo = "crc32",
   manifest_path = "xreactor/installer/manifest.lua",
-  installer_core_version = "2.1",
-  installer_core_hash = "35a9d58c",
-  installer_core_size_bytes = 127935
+  installer_core_version = "2.5",
+  installer_core_hash = "68ce7e95",
+  installer_core_size_bytes = 132647
 }
--- xreactor/installer/role_files.lua
+++ xreactor/installer/role_files.lua
@@ -1,8 +1,36 @@
 return {
-  MASTER = {},
-  RT = {},
-  ENERGY = {},
-  WATER = {},
-  FUEL = {},
-  REPROCESSOR = {},
+  MASTER = {
+    "xreactor/master/config.lua",
+    "xreactor/master/startup_sequencer.lua",
+    "xreactor/master/profiles.lua",
+    "xreactor/master/main.lua",
+    "xreactor/master/ui/alarms.lua",
+    "xreactor/master/ui/resources.lua",
+    "xreactor/master/ui/rt_dashboard.lua",
+    "xreactor/master/ui/overview.lua",
+    "xreactor/master/ui/alerts.lua",
+    "xreactor/master/ui/energy.lua",
+    "xreactor/master/ui/multiview.lua",
+    "xreactor/master/ui/widgets.lua",
+  },
+  RT = {
+    "xreactor/nodes/rt/config.lua",
+    "xreactor/nodes/rt/main.lua",
+  },
+  ENERGY = {
+    "xreactor/nodes/energy/config.lua",
+    "xreactor/nodes/energy/main.lua",
+  },
+  WATER = {
+    "xreactor/nodes/water/config.lua",
+    "xreactor/nodes/water/main.lua",
+  },
+  FUEL = {
+    "xreactor/nodes/fuel/config.lua",
+    "xreactor/nodes/fuel/main.lua",
+  },
+  REPROCESSOR = {
+    "xreactor/nodes/reprocessor/config.lua",
+    "xreactor/nodes/reprocessor/main.lua",
+  },
 }
