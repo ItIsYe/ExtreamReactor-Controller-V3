b7e74667b6fae7ff2ad0a6d8a584bbd3d552d27d
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-02-06 00:10:27 +0100

    Guard installer_core download when core already exists

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -8,7 +8,7 @@ return {
   installer_hash = "74f95fca",
   installer_size_bytes = 148480,
   files = {
-    { path = "installer", size_bytes = 6419, hash = "5a22c714" },
+    { path = "installer", size_bytes = 6327, hash = "35dfd9d2" },
     { path = "xreactor/core/bootstrap.lua", size_bytes = 8346, hash = "9a3af341" },
     { path = "xreactor/core/network.lua", size_bytes = 3241, hash = "870f6483" },
     { path = "xreactor/core/trends.lua", size_bytes = 1791, hash = "d01a6948" },
