53074f8d20c067eafa429471938b8bf893b82534
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-02-06 01:53:31 +0100

    Fix installer download loop to stream without content length

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -5,8 +5,8 @@ return {
   hash_algo = "crc32",
   installer_min_version = "1.4",
   installer_path = "xreactor/installer_core.lua",
-  installer_hash = "f3190d1d",
-  installer_size_bytes = 153564,
+  installer_hash = "516244fb",
+  installer_size_bytes = 154513,
   files = {
     { path = "installer", size_bytes = 2786, hash = "09398263" },
     { path = "xreactor/core/bootstrap.lua", size_bytes = 8346, hash = "9a3af341" },
@@ -40,7 +40,7 @@ return {
     { path = "xreactor/master/ui/energy.lua", size_bytes = 2752, hash = "0c81b1e5" },
     { path = "xreactor/shared/colors.lua", size_bytes = 332, hash = "445d12af" },
     { path = "xreactor/shared/constants.lua", size_bytes = 1318, hash = "10abc7ee" },
-    { path = "xreactor/installer_core.lua", size_bytes = 153564, hash = "f3190d1d" },
+    { path = "xreactor/installer_core.lua", size_bytes = 154513, hash = "516244fb" },
     { path = "xreactor/release.lua", size_bytes = 207, hash = "92420517" }
   }
 }
