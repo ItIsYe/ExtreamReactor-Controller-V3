ad5cf9a272e9ebaa0c4cb853be09edc9d31b2f16
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-17 21:00:34 +0200

    chore(release): v51→v52

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v51",
+  release_id = "beta-v52",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v51",
-  manifest_version = 51,
+  manifest_id = "manifest-v52",
+  manifest_version = 52,
   manifest_file_count = 125,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
