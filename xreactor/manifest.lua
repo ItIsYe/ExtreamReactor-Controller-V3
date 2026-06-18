b96f8e9ea98041b9829c10b8f98e08a06c3a262a
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-18 17:07:40 +0200

    chore(manifest): v70→v71 — installer_main log_mode fix

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,13 +1,13 @@
 -- xreactor/manifest.lua -- manifest-v45
 return {
-  manifest_version = 70,
-  manifest_id = "manifest-v70",
+  manifest_version = 71,
+  manifest_id = "manifest-v71",
   source_ref = "beta",
   hash_algo = "crc32",
 
   base_files = {
   { path = "installer_http.lua", size_bytes = 4688, hash = "ea6f7420", always=true},
-  { path = "installer_main.lua", size_bytes = 21451, hash = "2cb63a29", always=true},
+  { path = "installer_main.lua", size_bytes = 21824, hash = "db0ddafa", always=true},
   { path = "installer_manifest.lua", size_bytes = 4673, hash = "7aa0c027", always=true},
   { path = "installer_stage.lua", size_bytes = 7475, hash = "4c136db1", always=true},
   { path = "installer_startup.lua", size_bytes = 845, hash = "6939c5e9", always=true},
