4884b38e376f4c52e8fc3786952fe7539c3c0922
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-20 01:28:12 +0200

    chore(release): v84→v85

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v84",
+  release_id = "beta-v85",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v84",
-  manifest_version = 84,
+  manifest_id = "manifest-v85",
+  manifest_version = 85,
   manifest_file_count = 126,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
