a73e3a59e06cd403085224287d045159c5716df1
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-19 22:45:55 +0200

    chore(release): v77→v78

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v77",
+  release_id = "beta-v78",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v77",
-  manifest_version = 77,
+  manifest_id = "manifest-v78",
+  manifest_version = 78,
   manifest_file_count = 126,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
