dc2778b29cea052cd2810056f98a6e848325dfbe
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-22 22:18:16 +0200

    chore(manifest): v131→v132

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v45
 return {
-  manifest_version = 131,
-  manifest_id = "manifest-v131",
+  manifest_version = 132,
+  manifest_id = "manifest-v132",
   source_ref = "beta",
   hash_algo = "crc32",
 
