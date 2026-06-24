859a603a464a9892333162638c99bef64252e170
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-24 20:04:06 +0200

    chore(manifest): v147→v148 — STARTUP_PATH Fix

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v45
 return {
-  manifest_version = 147,
-  manifest_id = "manifest-v147",
+  manifest_version = 148,
+  manifest_id = "manifest-v148",
   source_ref = "beta",
   hash_algo = "crc32",
 
