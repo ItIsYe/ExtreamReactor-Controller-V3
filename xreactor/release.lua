63530c50d4560ff3d1f40b52a3576749125b982e
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-24 09:23:58 +0200

    chore(release): v138

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v137",
+  release_id = "beta-v138",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v137",
-  manifest_version = 137,
+  manifest_id = "manifest-v138",
+  manifest_version = 138,
   manifest_file_count = 131,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
