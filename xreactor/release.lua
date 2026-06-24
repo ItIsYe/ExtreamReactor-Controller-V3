e68b2b6c9e4c6b8921a9eaf8412df753a06ee60c
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-24 20:30:27 +0200

    chore(release): v150→v151

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v150",
+  release_id = "beta-v151",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v150",
-  manifest_version = 150,
+  manifest_id = "manifest-v151",
+  manifest_version = 151,
   manifest_file_count = 131,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
