2a5cade76ffc4d74260d8b4b9131cf9489a5990b
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-25 22:35:37 +0200

    chore(release): mark beta-v165 manifest metadata as non-strict

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -5,7 +5,7 @@ return {
   manifest_id = "manifest-v165",
   manifest_version = 165,
   manifest_file_count = 131,
-  hash_algo = "crc32",
+  hash_algo = "none",
   manifest_path = "xreactor/manifest.lua",
   installer_core_version = "2.0",
   installer_core_hash = "runtime-update",
