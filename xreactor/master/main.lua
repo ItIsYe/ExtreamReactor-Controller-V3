c368405a931c4a35e59201f5b0f03b192b8eff3c
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-26 19:27:47 +0200

    refactor(master/main.lua): Auto-Update Loop nach start.lua verschoben

--- xreactor/master/main.lua
+++ xreactor/master/main.lua
@@ -7,30 +7,4 @@ bootstrap.setup({
 
 local require = bootstrap.require
 local runtime_loop  = require('master.runtime_loop')
-local remote_update = require('core.remote_update')
-
--- Auto-Update Loop parallel zum Haupt-Loop starten.
--- Der Loop prüft periodisch ob eine neue Version auf GitHub vorhanden ist
--- und startet den Installer wenn auto_update=true in der Arming-Config.
-local function make_log()
-  local ok, utils = pcall(require, 'core.utils')
-  if ok and type(utils) == "table" and type(utils.log) == "function" then
-    return function(level, msg) utils.log("MASTER", msg, level) end
-  end
-  return function(level, msg) print("[" .. tostring(level) .. "] " .. tostring(msg)) end
-end
-
-local auto_log = make_log()
--- Defensiv: auto_check_loop nur wenn vorhanden
-local auto_loop = type(remote_update.auto_check_loop) == "function"
-  and remote_update.auto_check_loop(auto_log, 120)
-  or nil
-
-if auto_loop then
-  parallel.waitForAny(
-    function() runtime_loop.run() end,
-    auto_loop
-  )
-else
-  runtime_loop.run()
-end
+runtime_loop.run()
