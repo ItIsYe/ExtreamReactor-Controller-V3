b494d03384a5e221ec28c91d332c779b6191e9d1
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-26 11:15:48 +0200

    chore(release): v171

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v170",
+  release_id = "beta-v171",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v170",
-  manifest_version = 170,
+  manifest_id = "manifest-v171",
+  manifest_version = 171,
   manifest_file_count = 132,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
