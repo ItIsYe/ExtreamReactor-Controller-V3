78ad744720010eb51e9966d0f41b25c2a5e47369
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-26 09:27:48 +0200

    chore(manifest): v168→v169 — remote_update auto_check

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v165
 return {
-  manifest_version = 168,
-  manifest_id = "manifest-v168",
+  manifest_version = 169,
+  manifest_id = "manifest-v169",
   source_ref = "beta",
   hash_algo = "none",
 
@@ -36,7 +36,7 @@ return {
   { path = "core/ui_router.lua", size_bytes = 6305, hash = "e626dd67"},
   { path = "core/utils.lua", size_bytes = 23711, hash = "c57d619c"},
   { path = "core/auto_update.lua", size_bytes = 5174, hash = "332b3250" },
-  { path = "core/remote_update.lua", size_bytes = 4995, hash = "d92cc168"},
+  { path = "core/remote_update.lua", size_bytes = 9429, hash = "06196bc7"},
   { path = "core/alerts.lua", size_bytes = 7905, hash = "976d542c"},
   { path = "services/alert_service.lua", size_bytes = 11418, hash = "d04e6700"},
   { path = "services/comms_service.lua", size_bytes = 8562, hash = "4b2957a5"},
