1e969cbf7291b3602aa4def8d72233ffbcc42eb1
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-27 15:23:22 +0200

    chore(release): v195→v196

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v195",
+  release_id = "beta-v196",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v195",
-  manifest_version = 195,
+  manifest_id = "manifest-v196",
+  manifest_version = 196,
   manifest_file_count = 132,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
