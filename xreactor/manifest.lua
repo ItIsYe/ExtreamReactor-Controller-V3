5d4ed8a1ad04ed710f8f0a97f4b8b06ee24acfda
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-27 15:10:47 +0200

    chore(manifest): v192→v193 — Auto-Update auf Terminal

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v165
 return {
-  manifest_version = 192,
-  manifest_id = "manifest-v192",
+  manifest_version = 193,
+  manifest_id = "manifest-v193",
   source_ref = "beta",
   hash_algo = "none",
 
@@ -13,7 +13,7 @@ return {
   { path = "installer_startup.lua", size_bytes = 1487, hash = "99155a25", always=true},
   { path = "installer_storage.lua", size_bytes = 8866, hash = "c5809f7f", always=true},
   { path = "release.lua", size_bytes = 348, hash = "4f234312", always=true},
-  { path = "start.lua", size_bytes = 9979, hash = "8d3dad2f", always=true},
+  { path = "start.lua", size_bytes = 10181, hash = "339daf39", always=true},
   { path = "shared/build_info.lua", size_bytes = 1312, hash = "328286a9", always=true},
   { path = "shared/constants.lua", size_bytes = 1968, hash = "0c5fb837", always=true},
   { path = "adapters/monitor.lua", size_bytes = 5312, hash = "34f32460"},
