a85931aabcd61c7fdb028b2011002acd71edc7cf
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-27 19:15:25 +0200

    fix(manifest): release Hash v191

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -12,7 +12,7 @@ return {
   { path = "installer_stage.lua", size_bytes = 7475, hash = "4c136db1", always=true},
   { path = "installer_startup.lua", size_bytes = 1487, hash = "99155a25", always=true},
   { path = "installer_storage.lua", size_bytes = 8866, hash = "c5809f7f", always=true},
-  { path = "release.lua", size_bytes = 348, hash = "b0e97523", always=true},
+  { path = "release.lua", size_bytes = 348, hash = "22b4ed1b", always=true},
   { path = "start.lua", size_bytes = 10201, hash = "b8f867c8", always=true},
   { path = "shared/build_info.lua", size_bytes = 1312, hash = "328286a9", always=true},
   { path = "shared/constants.lua", size_bytes = 1968, hash = "0c5fb837", always=true},
