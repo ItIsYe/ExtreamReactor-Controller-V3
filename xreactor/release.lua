ad40f7bf23c078132af43678fd0f2944beeffc71
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-29 21:19:01 +0200

    bump: v221 (start.lua dofile fix)

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v220",
+  release_id = "beta-v221",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v220",
-  manifest_version = 220,
+  manifest_id = "manifest-v221",
+  manifest_version = 221,
   manifest_file_count = 144,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
