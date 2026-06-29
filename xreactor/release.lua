0e48d58f7ee10028940a4ea244877394da384fe2
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-29 20:45:33 +0200

    chore(release): v215→v216 — Auto-Update Test

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v215",
+  release_id = "beta-v216",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v215",
-  manifest_version = 215,
+  manifest_id = "manifest-v216",
+  manifest_version = 216,
   manifest_file_count = 144,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
