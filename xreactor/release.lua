6b2554d1325c27d41f9fed084aea208e286d2f15
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-29 20:29:23 +0200

    chore(release): v211→v212

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v211",
+  release_id = "beta-v212",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v211",
-  manifest_version = 211,
+  manifest_id = "manifest-v212",
+  manifest_version = 212,
   manifest_file_count = 144,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
