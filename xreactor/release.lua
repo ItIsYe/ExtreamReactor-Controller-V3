d41b04530a383c4134a6853d757f0c4c54dbeb2f
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-29 21:12:36 +0200

    test: bump to v220 for auto-updater test

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v219",
+  release_id = "beta-v220",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v219",
-  manifest_version = 219,
+  manifest_id = "manifest-v220",
+  manifest_version = 220,
   manifest_file_count = 144,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
