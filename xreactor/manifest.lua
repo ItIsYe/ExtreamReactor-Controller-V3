4b32cb614f15c4f14bb4eae09e66e03463454573
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-06-30 08:19:04 +0200

    fix: message_handlers.lua size_bytes -> 21973, bump manifest_version

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
--- xreactor/manifest.lua -- manifest-v224
+-- xreactor/manifest.lua -- manifest-v225
 return {
-  manifest_version = 224,
-  manifest_id = "manifest-v224",
+  manifest_version = 225,
+  manifest_id = "manifest-v225",
   source_ref = "beta",
   hash_algo = "none",
 
@@ -69,7 +69,7 @@ return {
     { path = "master/housekeeping.lua", size_bytes = 2801, hash = "ffbc176f", required_for={"MASTER"}},
     { path = "master/init_runtime.lua", size_bytes = 7307, hash = "eba62959", required_for={"MASTER"}},
     { path = "master/main.lua", size_bytes = 244, hash = "227a851a", required_for={"MASTER"}},
-    { path = "master/message_handlers.lua", size_bytes = 21372, hash = "ad77b73e", required_for={"MASTER"}},
+    { path = "master/message_handlers.lua", size_bytes = 21973, hash = "ad77b73e", required_for={"MASTER"}},
     { path = "master/monitor_sessions.lua", size_bytes = 8082, hash = "37d2cd5c", required_for={"MASTER"}},
     { path = "master/profiles.lua", size_bytes = 283, hash = "16f8e038", required_for={"MASTER"}},
     { path = "master/rt_sync.lua", size_bytes = 17394, hash = "e565fff1", required_for={"MASTER"}},
