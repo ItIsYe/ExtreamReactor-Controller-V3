7311cdbb85cb4dd73aaa28672cdc5be5e1f7a908
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-02 16:24:47 +0200

    bump: v264

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v263",
+  release_id = "beta-v264",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v263",
-  manifest_version = 263,
+  manifest_id = "manifest-v264",
+  manifest_version = 264,
   manifest_file_count = 139,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
