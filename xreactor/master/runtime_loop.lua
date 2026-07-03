8b6d9ff9778acd0b9baa354912505dd6de71ef42
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-03 11:05:35 +0200

    feat: wire up updates_ui in master ctx

--- xreactor/master/runtime_loop.lua
+++ xreactor/master/runtime_loop.lua
@@ -36,6 +36,7 @@ local function run_master()
   local alarms_ui           = require("master.ui.alarms")
   local alerts_ui           = require("master.ui.alerts")
   local maintenance_ui      = require("master.ui.maintenance")
+  local updates_ui          = require("master.ui.updates")
   local multiview_ui        = require("master.ui.multiview")
   local ui_controller_lib   = require("master.ui_controller")
   local ui_diagnostics      = require("master.ui_diagnostics")
@@ -89,7 +90,7 @@ local function run_master()
     monitor_manager = monitor_manager, multiview_ui = multiview_ui,
     overview_ui = overview_ui, energy_ui = energy_ui, rt_ui = rt_ui,
     resources_ui = resources_ui, alerts_ui = alerts_ui, alarms_ui = alarms_ui,
-    maintenance_ui = maintenance_ui,
+    maintenance_ui = maintenance_ui, updates_ui = updates_ui,
     comms_service = comms_service, service_manager = service_manager,
     rt_sync_coalescer_lib = rt_sync_coalescer_lib, alert_service_lib = alert_service_lib,
     telemetry_service = telemetry_service, control_service = control_service,
