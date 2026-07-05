ed6cdf548585749a03067b8c42d4da65f77c9a6b
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-05 02:07:15 +0200

    bump v291: multi-reactor RT overview fix

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v287
 return {
-  manifest_version = 290,
-  manifest_id = "manifest-v290",
+  manifest_version = 291,
+  manifest_id = "manifest-v291",
   source_ref = "beta",
   hash_algo = "crc32",
 
