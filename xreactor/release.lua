1784361e5670e82741a3b86a5d25e16ef5088511
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-05 20:53:47 +0200

    bump: v308

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v307",
+  release_id = "beta-v308",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v307",
-  manifest_version = 307,
+  manifest_id = "manifest-v308",
+  manifest_version = 308,
   manifest_file_count = 156,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
