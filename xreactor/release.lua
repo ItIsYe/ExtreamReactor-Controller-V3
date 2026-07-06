c158055062dca726f650ae1f0f823445b3ed07fc
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-06 20:10:16 +0200

    bump: v324

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v323",
+  release_id = "beta-v324",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v323",
-  manifest_version = 323,
+  manifest_id = "manifest-v324",
+  manifest_version = 324,
   manifest_file_count = 156,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
