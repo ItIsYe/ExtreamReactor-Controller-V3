40df217a65f94f554e6c5b9338258ceac06dee4e
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-07-07 09:26:36 +0200

    bump: v330

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v329",
+  release_id = "beta-v330",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v329",
-  manifest_version = 329,
+  manifest_id = "manifest-v330",
+  manifest_version = 330,
   manifest_file_count = 156,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
