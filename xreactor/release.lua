d83f9ae9608ee033a3a8753b10b91c599942d2ba
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-05 06:02:03 +0200

    chore(release): v483→v484

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v483",
+  release_id = "beta-v484",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v483",
-  manifest_version = 483,
+  manifest_id = "manifest-v484",
+  manifest_version = 484,
   manifest_file_count = 170,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
