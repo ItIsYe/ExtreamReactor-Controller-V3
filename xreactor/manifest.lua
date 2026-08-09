98ce5a5e172302a5ac1213b1f1fd8681e9908233
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-09 21:14:18 +0200

    chore(manifest): v508→v509 Edit-Button Fix

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v287
 return {
-  manifest_version = 508,
-  manifest_id = "manifest-v508",
+  manifest_version = 509,
+  manifest_id = "manifest-v509",
   source_ref = "beta",
   hash_algo = "crc32",
 
