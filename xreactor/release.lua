78f8611aa71110830555fc8fbcc4fb2850370c68
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-10 21:07:48 +0200

    chore(release): v523→v524

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,9 +1,9 @@
 return {
-  release_id = "beta-v523",
+  release_id = "beta-v524",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v523",
-  manifest_version = 523,
+  manifest_id = "manifest-v524",
+  manifest_version = 524,
   manifest_file_count = 174,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
