f6f1621eedf371240f208b57945c5ea2b287b9c7
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-11 19:45:35 +0200

    chore(release): v533→v534

--- xreactor/release.lua
+++ xreactor/release.lua
@@ -1,13 +1,13 @@
 return {
-  release_id = "beta-v533",
+  release_id = "beta-v534",
   commit_sha = "beta",
   source_ref = "beta",
-  manifest_id = "manifest-v533",
-  manifest_version = 533,
+  manifest_id = "manifest-v534",
+  manifest_version = 534,
   manifest_file_count = 176,
   hash_algo = "crc32",
   manifest_path = "xreactor/manifest.lua",
-  installer_core_version = "3.3",
+  installer_core_version = "3.2",
   installer_core_hash = "284f5f87",
   installer_core_size_bytes = 6447,
 }
