e0434492af8dcd8fcb1c9ef8ce43843d49766cf3
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-11 05:41:02 +0200

    chore(manifest): v527→v528

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,7 +1,7 @@
 -- xreactor/manifest.lua -- manifest-v521
 return {
-  manifest_version = 527,
-  manifest_id = "manifest-v527",
+  manifest_version = 528,
+  manifest_id = "manifest-v528",
   source_ref = "beta",
   hash_algo = "crc32",
 
@@ -16,7 +16,7 @@ return {
     { path = "installer/init.lua", size_bytes = 35757, hash = "207598f1", always = true },
     { path = "installer/journal.lua", always = true  },
     { path = "installer/plan_validator.lua", size_bytes = 6106, hash = "7ea9244c", always = true },
-    { path = "release.lua", always = true, size_bytes = 345, hash = "9e2c89cb" },
+    { path = "release.lua", always = true, size_bytes = 345, hash = "1a138485" },
     { path = "start.lua", size_bytes = 18876, hash = "483a917b", always = true },
     { path = "shared/build_info.lua", size_bytes = 1312, hash = "328286a9", always = true },
     { path = "shared/constants.lua", size_bytes = 4181, hash = "08d98202", always = true },
