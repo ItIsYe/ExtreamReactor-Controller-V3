0f55826aff7f6ccd1f32115d80d4f6f5fd262fef
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-11 20:15:37 +0200

    chore(manifest): v535→v536 Timeout-Fix rückgängig

--- xreactor/manifest.lua
+++ xreactor/manifest.lua
@@ -1,14 +1,14 @@
 -- xreactor/manifest.lua -- manifest-v533
 return {
-  manifest_version = 535,
-  manifest_id = "manifest-v535",
+  manifest_version = 536,
+  manifest_id = "manifest-v536",
   source_ref = "beta",
   hash_algo = "crc32",
 
   base_files = {
     { path = "services/heartbeat_service.lua", size_bytes = 3938, hash = "6b908df2" },
     { path = "services/auto_update_service.lua", size_bytes = 1688, hash = "1d9fcbc6" },
-    { path = "installer/http.lua", size_bytes = 3342, hash = "e9f336d9", always = true },
+    { path = "installer/http.lua", always = true  },
     { path = "installer/manifest.lua", size_bytes = 5794, hash = "faaa98fa", always = true },
     { path = "installer/stage.lua", size_bytes = 9164, hash = "b067f20b", always = true },
     { path = "installer/ui.lua", size_bytes = 2074, hash = "7bdd0eb9", always = true },
